f2f387fb741a10786881b945b6ce0b27c798d3bb
web3make <web3make@gmail.com>
2016-11-30 16:45:45 +0200

    Update auth.php

--- application/controllers/auth.php
+++ application/controllers/auth.php
@@ -60,6 +60,6 @@ class Auth extends CI_Controller
 
     function logout(){
         $this->session->sess_destroy();
-        redirect('/auth/login');
+        redirect($_SERVER['HTTP_REFERER']);
     }
 }
