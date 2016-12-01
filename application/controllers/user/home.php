b1c77b2a58deb42fba7cccec747b3b3df76646c6
web3make <web3make@gmail.com>
2016-12-01 09:14:48 +0200

    Update home.php

--- application/controllers/user/home.php
+++ application/controllers/user/home.php
@@ -7,6 +7,22 @@ class Home extends CI_Controller
         $this->load->library('pagination');
         
         $data = $this->base_model->default_user();
+
+		$config = array(
+            'base_url'    => base_url(), 
+    		'total_rows'  => $this->db->count_all('articles'),
+    		'first_link'  => 'first',
+    		'last_link'   => 'last',
+    		'next_link'   => '>',
+    		'prev_link'   => '<',
+    		'num_links'   => 3,
+    		'per_page'    => $data['settings'][2]->value,
+    		'uri_segment' => 1,
+        );
+                        
+		$this->pagination->initialize($config);
+            
+        $data['pages'] = $this->pagination->create_links();
         $data['articles'] = $this->base_model->pagination($id,$data['settings'][2]->value);
 		
         $this->load->view('user/home',$data);
